11d88dfd2b8c30281ef2d28c5a75e69c2dbc00bc
meghaagravat <meghaagravat16@gmail.com>
2026-01-06 18:22:12 +0530

    Add files via upload

---
+++ detatype.py
@@ -0,0 +1,28 @@
+
+x= 20
+print(x)
+x= 20.5
+print(x)
+x=1j
+print(x)
+x= ["apple","banana","cherry"]
+print(x)
+x= ("apple","banana","cherry")
+print(x)
+x= True
+print(x)
+x= False
+print(x)
+x = range(6)
+print(x)
+
+x = {"apple","banana","cherry"}
+print(x)
+x = frozenset({"apple","banana","cherry"})
+print(x)
+x = b"hello"
+print(x)
+x = bytearray(5)
+print(x)
+x = memoryview(bytes(5))
+print(x)
