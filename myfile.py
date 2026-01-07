97cec4d24c5baba3dfa12b043b6b905460e16d3d
meghaagravat <meghaagravat16@gmail.com>
2026-01-07 14:28:00 +0530

    Add files via upload

---
+++ myfile.py
@@ -0,0 +1,10 @@
+name = "megha"
+age = 17
+price = "34.99"
+print("my name is :",name)
+print("my age is :",age)
+print("price of this iteams :",price)
+print(age)
+print(type(name))
+print(type(age))
+print(type(price))
