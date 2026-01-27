484f2eaf53a5a7e441ba2f5484a75d8ef5b00976
meghaagravat <meghaagravat16@gmail.com>
2026-01-27 08:01:41 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,40 @@
+#arithmetic operator
+a = 2
+b = 6
+print(a+b)
+a = 2
+b = 5
+print(a-b)
+print(a*b)
+print(a%b)
+#relasnational operator
+a = 10
+b = 5
+print(a>b)
+print(a==b)
+# logic operator
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
+#boolean operator
+is_pass=True
+print(is_pass)
+#bitwise operator
+a = 5
+b = 3 
+print(a&b)
+print(a/b)
+#membership operator
+number=[1,2,3,4]
+print(2 in number)
+print(5 not in number)
+#identity operator
+a = 10
+b = 10
+print(a is b)
+print(a is not b)
+#unary operator
+a = 4
+print(-a)
