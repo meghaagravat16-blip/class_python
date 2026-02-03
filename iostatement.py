5e8adb1aaf526a4ce6207adfd48ccba32aba96c0
meghaagravat <meghaagravat16@gmail.com>
2026-02-03 07:45:50 +0530

    Add files via upload

---
+++ iostatement.py
@@ -0,0 +1,13 @@
+#input statement
+print("hello world")
+x=20
+print(x)
+x=20.5
+print(x)
+x=1j
+print(x)
+x=["apple","banana","cherry"]
+print(x)
+#input statement
+name: input(("enter your name"))
+print("hello",name)
